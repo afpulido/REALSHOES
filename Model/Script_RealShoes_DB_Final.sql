06c029fe902ed86bb6aa059fd6a1b18d6df5eb74
Andres Pulido <afpulido509@hotmail.com>
2023-03-01 17:58:48 -0500

    Triggers y Vistas

--- Model/Script_RealShoes_DB_Final.sql
+++ Model/Script_RealShoes_DB_Final.sql
@@ -6,9 +6,9 @@ use real_shoes;
 
 ### TABLA ROL, DESCRIBE EL ROL DESDE EL CUAL EL USUARIO VA A INTERACTUAR CON EL SISTEMA.
 create table rol(
-    idrol int(2) auto_increment primary key,
-    nombre_rol varchar(45),
-    descripcion_rol varchar(45),
+    Rol_Id int(2) primary key auto_increment,
+    nombre varchar(45),
+    descripcion varchar(45),
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
@@ -16,18 +16,18 @@ create table rol(
 );
 ### TABLA TIPO DOC, DESCRIBE EL DOCUMENTO DESDE EL CUAL EL USUARIO SE PUEDE IDENTIFICAR.
 create table tipo_doc(
-    idtd int(2) auto_increment primary key,
-    nombre_td varchar(45),
-    descripcion_td varchar(45),
+    Tipo_Doc_Id int(2) primary key auto_increment,
+    nombre varchar(45),
+    descripcion varchar(45),
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
 ### TABLA TIPO PERSONA DESCRIBE EL TIPO DE USUARIO (natural, jurídico, proveedor).
 create table tipo_persona(
-    id_tp int(2) auto_increment primary key,
-    nombretp varchar(45),
-    descripcion_tp varchar(45),
+    Tipo_persona_Id int(2) primary key auto_increment,
+    nombre varchar(45),
+    descripcion  varchar(45),
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
@@ -36,7 +36,7 @@ create table tipo_persona(
 ### TABLA PERSONA, ALMACENA LOS DATOS DEL LOG-IN.
 create table persona(
     id int primary key auto_increment,
-    idpersona int NOT NULL UNIQUE, ### revisar llave forenea
+    Persona_Id int NOT NULL UNIQUE, 
     nombre varchar(45),
     apellidos varchar(45),
     direccion varchar(45),
@@ -44,9 +44,9 @@ create table persona(
     contraseña varchar(45),
     telefono varchar(15) UNIQUE,
     email varchar(45) UNIQUE,
-    idtipodocp int,
-    idtipopersona int,
-    idrolp int,
+    Tipo_Doc_Id int,
+    Tipo_persona_Id int,
+    Rol_Id int,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
@@ -54,40 +54,42 @@ create table persona(
 
 ### TABLA PERSONA TRABAJA SEDE, ASOCIA UN EMPLEADO A UNA SEDE.
 create table Persona_trabaja_sede(
-    idpersonasede int primary key auto_increment,
-    idpersona int,
-    idsede int,
+    Persona_Idsede int primary key auto_increment,
+    Persona_Id int,
+    Sede_Id int,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
 ### TABLA PRODUCTOS, INFORMACIÓN SOBRE LOS PRODUCTOS
 Create table Producto(
-    Idproducto int(10) primary key auto_increment, 
+    Producto_Id int(10) primary key auto_increment, 
     Tipo varchar(45),
     Marca varchar(45) , 
     Coleccion_Temporada varchar(45),
     Genero varchar(45),
-    idtalla int,
+    Valor_Compra float,
+    Valor_Venta float,
+    Talla_Id int,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
 ### TABLA INVENTARIO, MUESTRA EL CODIGO DEL INVENTARIO Y SU RESPECTIVA SEDE.
 create table Inventario(
-    idplacainventario int(10) primary key auto_increment, 
-    idsede int UNIQUE,
+    Inventario_Id int(10) primary key auto_increment, 
+    Sede_Id int UNIQUE,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
 
 ### TABLA CONTENIDO_INVENTARIO, MUESTRA LAS EXISTENCIAS DE UN RESPECTIVO INVENTARIO.
-create table contenido_Inventario(
-    idci int primary key auto_increment,
-    idplacainventario int (10),
-    idproducto int(10),
-    stock int,
+create table Contenido_inventario(
+    Contenido_inventario_Id int primary key auto_increment,
+    Inventario_Id int (10),
+    Producto_Id int(10),
+    stock int default 0,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
@@ -95,82 +97,97 @@ create table contenido_Inventario(
 );
 ### TABLA Metodo_Pago, En esta tabla se muestra el método de pago empleado por el cliente
 create table Metodo_Pago (
-    IdMetodo_Pago int primary key, 
+    Metodo_pago_Id int primary key, 
     Tipo_pago varchar(45),
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
-### TABLA Persona_Producto_Pedido, En esta tabla se muestra la relación y como se asocia la el cliente con el producto adquirido.
-create table Persona_Producto_Pedido (
-    Idppp int primary key NOT NULL AUTO_INCREMENT, 
-    IdPersonapp int, 
-    IdProductopp int,
+### TABLA Persona_Producto, En esta tabla se muestra la relación y como se asocia el cliente con el producto adquirido.
+create table persona_producto(
+    persona_producto_id int primary key AUTO_INCREMENT, 
+    Persona_Id int, 
+    Producto_Id int,
+    Estado SET('SELECCIONADO','CANCELADO','FACTURADO') default'SELECCIONADO',
+    Tipo_Factura SET('COMPRA','VENTA') default 'VENTA',
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime 
 );
 ### TABLA Pedido, En esta tabla se muestra la información del pedido realizado por el cliente con su facturación
 create table Pedido (   
-    IdPedido int primary key NOT NULL AUTO_INCREMENT, 
-    Tipo_Factura varchar(45), 
-    Cantidad int, 
-    IdMetodopago int, 
-    Idppp int,
+    Pedido_Id int primary key AUTO_INCREMENT,
+    Cantidad int default 1, 
+    Valor_Total float default 0,
+    Metodo_pago_Id int default 1, 
+    persona_producto_id int,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
 ### TABLA talla
 create table talla(
-    idtalla int primary key auto_increment,
+    Talla_Id int primary key auto_increment,
     origen_talla varchar(45),
-    notalla int,
+    numero int,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
 ### TABLA FACTURA
 create table factura(
-    idfactura int primary key auto_increment,
-    fechacompraventa datetime,
-    cantidadproducto int,
-    valorunitario float,
-    valorcompleto float,
+    Factura_Id int primary key auto_increment,
     descuento float,
     impuesto float,
-    preciototal float,
-    idpedido int,
+    Pedido_Id int,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
+### TABLA VENTA
+create table venta(
+    Venta_Id int primary key auto_increment,
+    Factura_Id int,
+    Persona_Id int,
+    Producto_Id int(10),
+    cantidad int,
+    fecha_creacion DATETIME default current_timestamp
+);
+### TABLA compra
+create table compra(
+    Compras_Id int primary key auto_increment,
+    Factura_Id int,
+    Persona_Id int,
+    Producto_Id int(10),
+    cantidad int,
+    fecha_creacion DATETIME default current_timestamp
+);
 ### tabla sede
 create table sede (
-    idsede int primary key auto_increment,
-    nombresede varchar(45),
-    direccionsede varchar(45),
-    telefonosede varchar(45),
-    emailsede varchar(45),
-    idciudadsede int,
+    Sede_Id int primary key auto_increment,
+    nombre varchar(45),
+    direccion varchar(45),
+    telefono varchar(45),
+    email varchar(45),
+    Ciudad_Id int,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
 ### tabla pais
 create table pais(
-    idpais int primary key auto_increment,
-    nombrepais varchar(45),
-    capitalpais varchar(45),
+    Pais_Id int primary key auto_increment,
+    nombre varchar(45),
+    capital varchar(45),
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
 );
 ##tabla ciudad
 create table ciudad(
-    idciudad int primary key auto_increment,
-    nombreciudad varchar(45),
-    idpaisc int,
+    Ciudad_Id int primary key auto_increment,
+    nombre varchar(45),
+    Pais_Id int,
     fecha_creacion datetime default current_timestamp,
     ultima_modificacion datetime default current_timestamp,
     fecha_eliminacion datetime
@@ -180,25 +197,34 @@ create table ciudad(
 
 ### LLAVES FORANEAS
 
-ALTER table PERSONA ADD constraint fk_persona_rol foreign key(idrolp) references  rol(idrol);
-ALTER table PERSONA ADD constraint fk_persona_tp foreign key(idtipopersona) references  tipo_persona(id_tp);
-ALTER table PERSONA ADD constraint fk_persona_td foreign key(idtipodocp) references  tipo_doc(idtd);
+    ALTER table PERSONA ADD constraint fk_persona_rol foreign key(Rol_Id) references  rol(Rol_Id);
+    ALTER table PERSONA ADD constraint fk_persona_tipo_persona foreign key(Tipo_persona_Id) references  tipo_persona(Tipo_persona_Id);
+    ALTER table PERSONA ADD constraint fk_persona_tipo_doc foreign key(Tipo_Doc_Id) references  tipo_doc(Tipo_Doc_Id);
 
-alter table persona_producto_pedido add constraint fk_ppp_per foreign key (IdPersonapp) references persona(id);
-alter table persona_producto_pedido add constraint fk_ppp_ped foreign key (IdProductopp) references producto(idproducto);
+    alter table persona_producto add constraint fk_persona_producto_persona foreign key (Persona_Id) references persona(Persona_Id);
+    alter table persona_producto add constraint fk_persona_producto_producto foreign key (Producto_Id) references producto(Producto_Id);
 
-alter table pedido add constraint fk_pedido_mp foreign key(idmetodopago) references metodo_pago(IdMetodo_Pago);
-alter table pedido add constraint fk_pedido_ppp foreign key(idppp) references persona_producto_pedido(idppp);
+    alter table pedido add constraint fk_pedido_metodo_pago foreign key(Metodo_pago_Id) references metodo_pago(Metodo_pago_Id);
+    alter table pedido add constraint fk_pedido_persona_producto_pedido foreign key(persona_producto_id) references persona_producto(persona_producto_id);
+    
 
-alter table  factura add constraint fk_factura_ped foreign key(idpedido) references pedido(idpedido);
+    alter table  factura add constraint fk_factura_pedido foreign key(Pedido_Id) references pedido(Pedido_Id);
 
-alter table producto add constraint fk_producto_talla foreign key(idtalla) references talla(idtalla);
+    alter table producto add constraint fk_producto_talla foreign key(Talla_Id) references talla(Talla_Id);
 
-alter table inventario add constraint fk_inventario_sede foreign key (idsede) references sede(idsede);
+    alter table inventario add constraint fk_inventario_sede foreign key (Sede_Id) references sede(Sede_Id);
 
-alter table sede add constraint fk_sede_ciudad foreign key (idciudadsede) references ciudad(idciudad);
+    alter table sede add constraint fk_sede_ciudad foreign key (Ciudad_Id) references ciudad(Ciudad_Id);
 
-alter table ciudad add constraint fk_ciudad_pais foreign key (idpaisc) references pais(idpais);
+    alter table ciudad add constraint fk_ciudad_pais foreign key (Pais_Id) references pais(Pais_Id);
 
-alter table contenido_Inventario add constraint fk_contenidoinventario_inventario foreign key (idplacainventario) references inventario(idplacainventario);
-alter table contenido_Inventario add constraint fk_contenidoinventario_producto foreign key (idproducto) references producto(idproducto);
+    alter table contenido_Inventario add constraint fk_contenidoinventario_inventario foreign key (Inventario_Id) references inventario(Inventario_Id);
+    alter table contenido_Inventario add constraint fk_contenidoinventario_producto foreign key (Producto_Id) references producto(Producto_Id);
+
+    alter table venta add constraint fk_venta_factura foreign key (Factura_Id) references factura(Factura_Id);
+    alter table venta add constraint fk_venta_persona foreign key (Persona_Id) references persona(Persona_Id);
+    alter table venta add constraint fk_venta_producto foreign key (Producto_Id) references producto(Producto_Id);
+
+    alter table compra add constraint fk_compra_factura foreign key (Factura_Id) references factura(Factura_Id);
+    alter table compra add constraint fk_compra_persona foreign key (Persona_Id) references persona(Persona_Id);
+    alter table compra add constraint fk_compra_producto foreign key (Producto_Id) references producto(Producto_Id);
--- Model/Script_RealShoes_DB_Triggers.sql
+++ Model/Script_RealShoes_DB_Triggers.sql
@@ -1,34 +1,214 @@
 use real_shoes;
 
-DROP TRIGGER if exists after_compra_actualizacion_inventario;
+/* TRIGGER QUE AÑADE AL INVENTARIO UN NUEVO PRODUCTO REGISTRADO CON STOCK 0 */
+    DROP TRIGGER if exists after_añadir_producto_insert_contenido_inventario;
 
-DELIMITER //
+    DELIMITER //
 
-CREATE TRIGGER after_compra_actualizacion_inventario
-AFTER INSERT ON factura
-FOR EACH ROW
-BEGIN 
-    /* 1. saber el tipo de factura
-        2. la sede que la expide
-        3. el id del producto
-        4. las cantidades*/
-   SET @idpedido = (SELECT  idpedido FROM factura 
+    CREATE TRIGGER after_añadir_producto_insert_contenido_inventario
+    AFTER INSERT ON producto
+    FOR EACH ROW
+        BEGIN
+            INSERT INTO contenido_inventario(inventario_id,producto_id) values (1,new.producto_id),
+                                                                        (2,new.producto_id);
+        END;
+        //
+
+    DELIMITER ;
+
+/* TRIGGER QUE DESPUES DE UNA VENTA ACTUALIZA EL INVENTARIO */  
+
+    DROP TRIGGER if exists after_compra_actualizacion_inventario;
+
+    DELIMITER //
+
+    CREATE TRIGGER after_compra_actualizacion_inventario
+    AFTER INSERT ON factura
+    FOR EACH ROW
+    BEGIN 
+    /* Para actualizar el inventario necesitamos saber si la factura es de 'COMPRA' o 'VENTA' para añadir
+     o restar unidades, el inventario de la respectiva sede que realiza la operacion, el id del producto y
+     las cantidades, en las siguientes lineas hallamos las respuestas */
+
+    /* Query para hallar el id del pedido de la última factura */
+        SET @pedido_id = (SELECT pedido_id FROM factura 
         ORDER BY fecha_creacion DESC limit 1);
 
     /* Tipo de factura */
-    SELECT  Tipo_Factura FROM pedido
-        INNER JOIN factura ON factura.idpedido = pedido.idpedido WHERE factura.idpedido =(@idpedido);
+        SET @tipo = (SELECT  Tipo_Factura FROM persona_producto
+        INNER JOIN pedido ON pedido.persona_producto_id = persona_producto.persona_producto_id WHERE pedido.pedido_id =(@pedido_id));
 
-    /* Sede */
-    SET @Idppp = (SELECT  Idppp FROM pedido
-        INNER JOIN factura ON factura.idpedido = pedido.idpedido WHERE factura.idpedido =(@idpedido));
+    /* Inventario */
+        SET @persona_producto_id = (SELECT  persona_producto_id FROM pedido
+        INNER JOIN factura ON factura.pedido_id = pedido.pedido_id WHERE factura.pedido_id =(@pedido_id));
     
-    SET @Idpersona = (SELECT ppp.idpersonapp FROM persona_producto_pedido AS ppp
-        INNER JOIN pedido AS p ON p.Idppp = ppp.Idppp WHERE p.Idppp = (@Idppp));
+        SET @persona_id = (SELECT ppp.persona_id FROM persona_producto AS ppp
+        INNER JOIN pedido AS p ON p.persona_producto_id = ppp.persona_producto_id WHERE p.persona_producto_id = (@persona_producto_id));
 
-    SET @sede = (SELECT pts.idsede FROM persona_trabaja_sede AS pts
-        INNER JOIN persona_producto_pedido AS ppp ON pts.idpersona = ppp.idpersonapp WHERE ppp.idpersonapp =(@Idpersona) limit 1);
-END;
-//
+        SET @sede = (SELECT pts.sede_id FROM persona_trabaja_sede AS pts
+        INNER JOIN persona_producto AS ppp ON pts.persona_id = ppp.persona_id WHERE ppp.persona_id =(@persona_id) limit 1);
 
-DELIMITER ;
+        SET @Inventario_id = (SELECT inv.inventario_id FROM inventario AS inv
+        INNER JOIN sede ON sede.sede_id = inv.sede_id WHERE sede.sede_id = (@sede));
+
+    /* ID producto */
+        SET @producto_id = (SELECT ppp.producto_id FROM persona_producto AS ppp
+        INNER JOIN pedido AS p ON p.persona_producto_id = ppp.persona_producto_id WHERE p.persona_producto_id = (@persona_producto_id));
+    
+    /* Cantidades */
+        SET @unidades = (SELECT  Cantidad FROM pedido
+        INNER JOIN factura ON factura.pedido_id = pedido.pedido_id WHERE factura.pedido_id =(@pedido_id));
+
+    /* condicional para añadir o restar unidades al inventario dependiendo del tipo_factura */
+    IF @tipo = 'COMPRA' THEN 
+        UPDATE contenido_inventario SET stock = stock + @unidades WHERE producto_id = @producto_id and Inventario_id = @Inventario_id;
+    ELSE
+        UPDATE contenido_inventario SET stock = stock - @unidades WHERE producto_id = @producto_id and Inventario_id = @Inventario_id; 
+    END IF;
+    END;
+    //
+
+    DELIMITER ;
+
+/* TRIGGER PARA REGISTRAR UNA VENTA DESPUES DE REALIZAR FACTURA */
+    DROP TRIGGER if exists after_compra_insertar_registro_venta;
+
+    DELIMITER //
+
+    CREATE TRIGGER after_compra_insertar_registro_venta
+    AFTER INSERT ON factura    
+    FOR EACH ROW 
+    BEGIN
+    /* Para hacer una inserción en la tabla venta necesitamos conocer la factura_id,
+    la persona que registro la venta, la sede desde donde se realizo la transaccion el producto que se vendió y la cantidad */
+        
+    /* Factura_Id */ 
+        SET @factura_id = (SELECT factura_id FROM factura ORDER BY fecha_creacion DESC limit 1);
+
+    /* Tipo_factura */
+        SET @pedido_id = (SELECT pedido_id from factura ORDER BY fecha_creacion DESC Limit 1);
+
+        SET @tipo = (SELECT  Tipo_Factura FROM persona_producto
+        INNER JOIN pedido ON pedido.persona_producto_id = persona_producto.persona_producto_id WHERE pedido.pedido_id =(@pedido_id));
+
+    /* Inventario_Id */
+        SET @persona_producto_id = (SELECT persona_producto_id FROM pedido
+        INNER JOIN factura ON factura.pedido_id = pedido.pedido_id WHERE factura.pedido_id =(@pedido_id));
+        
+        SET @persona_id = (SELECT ppp.persona_id from persona_producto as ppp
+        INNER JOIN pedido as p ON p.persona_producto_id = ppp.persona_producto_id WHERE p.persona_producto_id =(@persona_producto_id)); 
+
+        SET @sede = (SELECT pts.sede_id FROM persona_trabaja_sede AS pts
+        INNER JOIN persona_producto AS ppp ON pts.persona_id = ppp.persona_id WHERE ppp.persona_id =(@persona_id) limit 1);
+
+        SET @Inventario_id = (SELECT inv.inventario_id FROM inventario AS inv
+        INNER JOIN sede ON sede.sede_id = inv.sede_id WHERE sede.sede_id = (@sede));
+        
+    /* ID producto */
+        SET @producto_id = (SELECT ppp.producto_id FROM persona_producto AS ppp
+        INNER JOIN pedido AS p ON p.persona_producto_id = ppp.persona_producto_id WHERE p.persona_producto_id = (@persona_producto_id));
+
+    /* Cantidades */
+        SET @unidades = (SELECT  Cantidad FROM pedido
+        INNER JOIN factura ON factura.pedido_id = pedido.pedido_id WHERE factura.pedido_id =(@pedido_id));
+    
+    /* condicional que registra la venta o la compra dependiendo del tipo_factura */
+    IF @tipo = 'COMPRA' THEN
+        INSERT INTO compra(Factura_Id,persona_id,producto_id,cantidad) values (@factura_id,@persona_id,@producto_id,@unidades);
+    ELSE
+        INSERT INTO venta(Factura_Id,persona_id,producto_id,cantidad) values (@factura_id,@persona_id,@producto_id,@unidades);
+    END IF;
+    END;
+    //
+
+    DELIMITER ;
+
+/* Trigger que actualiza el valor del pedido dependiendo de las cantidades y el producto seleccionado */
+
+    DROP TRIGGER if exists after_persona_producto_actualizar_pedido;
+
+    DELIMITER //
+
+    CREATE TRIGGER after_persona_producto_actualizar_pedido
+    AFTER INSERT on persona_producto
+    FOR EACH ROW
+    BEGIN
+        SET @Cantidad = (SELECT Count(CASE WHEN Persona_id AND Estado = 'SELECCIONADO' AND Tipo_Factura ='VENTA' THEN 1 END) AS contador FROM persona_producto 
+                                GROUP BY Persona_id );
+        SET @persona_id = (SELECT pp.Persona_Id FROM persona_producto AS pp
+                                    INNER JOIN pedido AS p on 
+                                        p.persona_producto_id = pp.persona_producto_id);
+
+        SET @persona_producto_id = (SELECT persona_producto_id FROM persona_producto 
+                                        WHERE persona_id = (@persona_id) 
+                                            AND Estado = 'SELECCIONADO' 
+                                                ORDER BY fecha_creacion ASC
+                                                    Limit 1); 
+
+        SET @valor_total = (SELECT SUM(Valor_Venta) FROM producto AS p
+                                INNER JOIN persona_producto AS pp on
+                                    p.producto_id = pp.producto_id
+                                        WHERE pp.persona_id = (@persona_id) 
+                                            AND Estado = 'SELECCIONADO' 
+                                                AND Tipo_Factura = 'VENTA');   
+        IF @Cantidad > 1 THEN 
+            UPDATE pedido SET VALOR_TOTAL = (@valor_total) 
+                WHERE persona_producto_id =(@persona_producto_id);
+            
+            UPDATE pedido SET Cantidad =(@cantidad) 
+            WHERE persona_producto_id =(@persona_producto_id); 
+        END IF;
+    END;
+    //
+
+    DELIMITER ;
+
+/* TRIGGER QUE DESPUES DE UNA INSERCION A LA TABLA PEDIDO ACTUALIZA EL VALOR TOTAL */
+
+    /* DROP TRIGGER if exists after_insert_pedido_actualizar_valor_pedido
+
+    DELIMITER //    
+    CREATE TRIGGER after_insert_pedido_actualizar_valor_pedido 
+    AFTER INSERT on pedido
+    FOR EACH ROW
+    BEGIN
+        SET @valor_venta = (SELECT VALOR_VENTA FROM producto 
+            INNER JOIN persona_producto on
+                producto.producto_id = persona_producto.producto_id
+                    INNER JOIN pedido on
+                        pedido.persona_producto_id = persona_producto.persona_producto_id
+                            INNER JOIN persona on
+                                persona.persona_id = persona_producto.persona_id
+                                    WHERE persona_producto.persona_id = persona.persona_id 
+                                        AND persona_producto.Tipo_Factura = 'VENTA' 
+                                            AND persona_producto.Estado = 'SELECCIONADO'
+                                                ORDER BY persona_producto.fecha_creacion DESC
+                                                    LIMIT 1);
+        SET @valor_compra = (SELECT VALOR_COMPRA FROM producto 
+            INNER JOIN persona_producto on
+                producto.producto_id = persona_producto.producto_id
+                    INNER JOIN pedido on
+                        pedido.persona_producto_id = persona_producto.persona_producto_id
+                            INNER JOIN persona on
+                                persona.persona_id = persona_producto.persona_id
+                                    WHERE persona_producto.persona_id = persona.persona_id 
+                                        AND persona_producto.Tipo_Factura = 'COMPRA' 
+                                            AND persona_producto.Estado = 'SELECCIONADO'
+                                                ORDER BY persona_producto.fecha_creacion DESC
+                                                    LIMIT 1);
+        SET @tipo_factura = (SELECT Tipo_Factura FROM persona_producto
+            INNER JOIN pedido on 
+                pedido.persona_producto_id = persona_producto.persona_producto_id
+                    ORDER BY persona_producto.fecha_creacion DESC 
+                        LIMIT 1);
+
+        IF @Tipo_Factura = 'VENTA' THEN
+            UPDATE pedido SET Valor_Total = (@valor_venta);
+        ELSE 
+            UPDATE pedido SET Valor_Total = (@valor_compra);
+        END IF;
+    END;
+    //
+
+    DELIMITER ; */
+
