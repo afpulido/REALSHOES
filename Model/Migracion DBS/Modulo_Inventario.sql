838adc97581e7dcf4c52b9a3c470b8616cd653b3
Andres Pulido <afpulido509@hotmail.com>
2023-03-24 22:37:12 -0500

    Migracion Modulo Inventario

--- Model/Migracion DBS/Modulo_Inventario.sql
+++ Model/Migracion DBS/Modulo_Inventario.sql
@@ -1,17 +1,17 @@
 use real_shoes;
-### TABLAS
-    ### TABLA TALLA, ALMACENA LAS TALLAS Y SU ORIGEN. 
+--- TABLAS
+    --- TABLA TALLA, ALMACENA LAS TALLAS Y SU ORIGEN. 
     create table talla(
-        Talla_Id int primary key auto_increment,
+        Talla_Id int primary key IDENTITY(1,1),
         pais_id int,
         numero int,
         fecha_creacion datetime default current_timestamp,
         ultima_modificacion datetime default current_timestamp,
         fecha_eliminacion datetime
     );
-    ### TABLA PRODUCTOS, INFORMACIÓN SOBRE LOS PRODUCTOS
-    Create table Producto(
-        Producto_Id int(10) primary key auto_increment, 
+    --- TABLA PRODUCTOS, INFORMACIÓN SOBRE LOS PRODUCTOS
+    Create table producto(
+        Producto_Id int primary key IDENTITY(1,1), 
         Tipo varchar(45),
         Marca varchar(45) , 
         Coleccion_Temporada varchar(45),
@@ -23,20 +23,20 @@ use real_shoes;
         ultima_modificacion datetime default current_timestamp,
         fecha_eliminacion datetime
     );
-    ### TABLA INVENTARIO, MUESTRA EL CODIGO DEL INVENTARIO Y SU RESPECTIVA SEDE.
-    create table Inventario(
-        Inventario_Id int(10) primary key auto_increment, 
+    --- TABLA INVENTARIO, MUESTRA EL CODIGO DEL INVENTARIO Y SU RESPECTIVA SEDE.
+    create table inventario(
+        Inventario_Id int primary key IDENTITY(1,1), 
         Sede_Id int UNIQUE,
         fecha_creacion datetime default current_timestamp,
         ultima_modificacion datetime default current_timestamp,
         fecha_eliminacion datetime
     );
 
-    ### TABLA CONTENIDO_INVENTARIO, ALMACENA LAS EXISTENCIAS DE UN RESPECTIVO INVENTARIO.
-    create table Contenido_inventario(
-        Contenido_inventario_Id int primary key auto_increment,
-        Inventario_Id int (10),
-        Producto_Id int(10),
+    --- TABLA CONTENIDO_INVENTARIO, ALMACENA LAS EXISTENCIAS DE UN RESPECTIVO INVENTARIO.
+    create table contenido_inventario(
+        contenido_inventario_Id int primary key IDENTITY(1,1),
+        Inventario_Id int,
+        Producto_Id int,
         stock int default 0,
         fecha_creacion datetime default current_timestamp,
         ultima_modificacion datetime default current_timestamp,
@@ -44,10 +44,10 @@ use real_shoes;
 
     );
 
-### TABLAS CON DATOS ANTIGUOS
-    ### TABLA ANTIGUO_PERSONA, ALMACENA LOS DATOS ANTIGUOS DE LA TABLA PRODUCTOS.
+--- TABLAS CON DATOS ANTIGUOS
+    --- TABLA ANTIGUO_PERSONA, ALMACENA LOS DATOS ANTIGUOS DE LA TABLA PRODUCTOS.
     Create table antiguo_producto(
-        Producto_Id int(10) primary key auto_increment, 
+        Producto_Id int primary key IDENTITY(1,1), 
         Tipo varchar(45),
         Marca varchar(45) , 
         Coleccion_Temporada varchar(45),
@@ -60,11 +60,11 @@ use real_shoes;
         fecha_eliminacion datetime
     );
 
-    ### TABLA ANTIGUO_CONTENIDO_INVENTARIO, ALMACENA LOS DATOS ANTIGUOS DE LA TABLA CONTENIDO_INVENTARIO.
+    --- TABLA ANTIGUO_CONTENIDO_INVENTARIO, ALMACENA LOS DATOS ANTIGUOS DE LA TABLA CONTENIDO_INVENTARIO.
     create table antiguo_contenido_inventario(
-        Contenido_inventario_Id int primary key auto_increment,
-        Inventario_Id int (10),
-        Producto_Id int(10),
+        Contenido_inventario_Id int primary key IDENTITY(1,1),
+        Inventario_Id int,
+        Producto_Id int,
         stock int default 0,
         fecha_creacion datetime default current_timestamp,
         ultima_modificacion datetime default current_timestamp,
@@ -73,7 +73,7 @@ use real_shoes;
     );
 
 
-### LLAVES FORANEAS
+--- LLAVES FORANEAS
 
     alter table talla add constraint fk_talla_pais foreign key (pais_id) references pais(pais_id);
 
@@ -84,16 +84,16 @@ use real_shoes;
     alter table contenido_Inventario add constraint fk_contenidoinventario_inventario foreign key (Inventario_Id) references inventario(Inventario_Id);
     alter table contenido_Inventario add constraint fk_contenidoinventario_producto foreign key (Producto_Id) references producto(Producto_Id);
 
-### LLAVES FORANEAS DATOS ANTIGUOS
+--- LLAVES FORANEAS DATOS ANTIGUOS
 
     alter table antiguo_producto add constraint fk_antiguo_producto_talla foreign key(Talla_Id) references talla(Talla_Id);
 
     alter table antiguo_contenido_Inventario add constraint fk_antiguo_contenido_inventario_inventario foreign key (Inventario_Id) references inventario(Inventario_Id);
     alter table antiguo_contenido_Inventario add constraint fk_antiguo_contenido_inventario_producto foreign key (Producto_Id) references producto(Producto_Id);
 
-### DATOS
+--- DATOS
 
-    ### DATOS TALLA
+    --- DATOS TALLA
     insert into talla (pais_id,numero) values 
         (41,38),
         (41,39),
@@ -108,110 +108,97 @@ use real_shoes;
         (59,40),
         (59,41);
 
-    ### DATOS PRODUCTO
+    --- DATOS PRODUCTO
     /* insert into producto(tipo,Marca,Coleccion_temporada,genero,Valor_Compra,Valor_Venta,talla_id) values
         ('Zapatilla','Adidas','Verano','Femenino',1,2,2),
         ('Tenis','Brahma','Verano','Masculino',1,2,3); */
 
-    ### DATOS INVENTARIO
+    --- DATOS INVENTARIO
     insert into inventario(Sede_Id) values
         (1),
         (2);
     
-### TRIGGERS
+--- TRIGGERS
     /* TRIGGER QUE AÑADE AL CONTENIDO_INVENTARIO UN NUEVO PRODUCTO REGISTRADO CON STOCK 0 */
-        DROP TRIGGER if exists after_añadir_producto_insert_contenido_inventario;
-
-        DELIMITER //
-
-        CREATE TRIGGER after_añadir_producto_insert_contenido_inventario
-        AFTER INSERT ON producto
-        FOR EACH ROW
-            BEGIN
-                SET @inventarios = (SELECT COUNT(inventario_id) FROM inventario);
-                SET @contador = 1;
-                WHILE @contador <= @inventarios DO    
-                    INSERT INTO contenido_inventario(inventario_id,producto_id) values (@contador,new.producto_id);
-                    SET @contador = @contador + 1;
-                END WHILE;
+       GO
+	   CREATE TRIGGER after_añadir_producto_insert_contenido_inventario
+        ON producto
+        FOR INSERT 
+        AS 
+        BEGIN
+            DECLARE @inventarios INT, @contador INT
+            SELECT @inventarios = COUNT(inventario_id) FROM inventario
+            SET @contador = 1;
+            WHILE @contador <= @inventarios
+            BEGIN    
+                INSERT INTO contenido_inventario(inventario_id, producto_id)
+                SELECT @contador, producto_id
+                FROM inserted;
+                SET @contador = @contador + 1;
             END;
-            //
-
-        DELIMITER ;
-    
+        END;    
     /* TRIGGER QUE INSERTA DATOS ANTIGUOS A LA TABLA ANTIGUO_PRODUCTO, DESPUES DE UNA ACTUALIZACION 
     EN LA TABLA PRODUCTO */
 
-        DROP TRIGGER if exists after_update_producto
-
-        DELIMITER //
-
+        GO
         CREATE TRIGGER after_update_producto
-        AFTER UPDATE ON producto
-        FOR EACH ROW
-            BEGIN 
-                INSERT INTO antiguo_producto (Producto_id,Tipo,Marca,Coleccion_Temporada,Genero,Valor_Compra,Valor_Venta,Talla_Id,fecha_creacion,ultima_modificacion)
-                    values(old.Producto_id,old.Tipo,Marca,old.Coleccion_Temporada,old.Genero,old.Valor_Compra,old.Valor_Venta,old.Talla_Id,old.fecha_creacion,now());
-            END;
-            //
-
-        DELIMITER ;   
+        ON producto
+        FOR UPDATE 
+        AS 
+        BEGIN
+            INSERT INTO antiguo_producto (Producto_id,Tipo,Marca,Coleccion_Temporada,Genero,Valor_Compra,Valor_Venta,Talla_Id,fecha_creacion,ultima_modificacion)
+            SELECT Producto_id,Tipo,Marca,Coleccion_Temporada,Genero,Valor_Compra,Valor_Venta,Talla_Id,fecha_creacion, GETDATE()
+            FROM deleted;
+        END;
 
     /* TRIGGER QUE INSERTA DATOS ANTIGUOS A LA TABLA ANTIGUO_CONTENIDO_INVENTARIO, DESPUES DE UNA ACTUALIZACION
     EN LA TABLA CONTENIDO_INVENTARIO */     
 
-        DROP TRIGGER if exists after_update_contenido_inventario
-
-        DELIMITER //
-
+        GO
         CREATE TRIGGER after_update_contenido_inventario
-        AFTER UPDATE ON contenido_inventario
-        FOR EACH ROW
-            BEGIN
-                INSERT INTO antiguo_contenido_Inventario(inventario_id,producto_id,stock,fecha_creacion,ultima_modificacion)
-                    values(old.inventario_id,old.producto_id,old.stock,old.fecha_creacion, now());
-            END;
-            //
-        
-        DELIMITER ;
-    
+        ON contenido_inventario
+        FOR UPDATE 
+        AS 
+        BEGIN
+            INSERT INTO antiguo_contenido_Inventario(inventario_id,producto_id,stock,fecha_creacion,ultima_modificacion)
+            SELECT inventario_id,producto_id,stock,fecha_creacion, GETDATE()
+            FROM deleted;
+        END;
+--- VISTAS
+	/* VISTA QUE MUESTRA LOS PRODUCTOS CON STOCK BAJO */
+        GO
+		CREATE VIEW stock_bajo_vw AS 
+            SELECT c.Inventario_Id AS Codigo_Inventario, c.Producto_id AS Codigo_producto, 
+                        p.tipo + ' ' + p.marca + ' ' + p.Coleccion_temporada + ' ' + p.genero AS producto,
+                            pa.nombre AS Origen, t.numero AS Talla, c.stock  
+                                FROM contenido_Inventario AS c
+                                    INNER JOIN producto AS p ON c.producto_id = p.producto_id
+                                        INNER JOIN talla AS t ON t.talla_id = p.talla_id
+                                            INNER JOIN pais AS pa ON pa.pais_id = t.pais_id
+                                                WHERE c.stock <= 3 AND c.stock > 0;
 
-### VISTAS
-    /* VISTA QUE MUESTRA LOS PRODUCTOS CON STOCK BAJO */
-        CREATE OR REPLACE VIEW stock_bajo_vw AS 
-         SELECT c.Inventario_Id AS Codigo_Inventario,c.Producto_id AS Codigo_producto, 
-                    CONCAT(p.tipo," ", p.marca," ", p.Coleccion_temporada," ", p.genero) AS producto,
-                        pa.nombre AS Origen, t.numero AS Talla, c.stock  
-                            FROM contenido_Inventario AS c
-                                INNER JOIN producto AS p ON
-                                 c.producto_id = p.producto_id
-                                    INNER JOIN talla AS t ON
-                                        t.talla_id = p.talla_id
-                                            INNER JOIN pais AS pa ON
-                                                pa.pais_id = t.pais_id
-                                                    WHERE c.stock <= 3 AND c.stock > 0;
     /* VISTA QUE MUESTRA LOS PRODUCTOS CON STOCK 0 */
-        CREATE OR REPLACE VIEW stock_cero_vw AS 
-         SELECT c.Inventario_Id AS Codigo_Inventario,c.Producto_id AS Codigo_producto, 
-                    CONCAT(p.tipo," ", p.marca," ", p.Coleccion_temporada," ", p.genero) AS producto,
-                        pa.nombre AS Origen, t.numero AS Talla, c.stock  
-                            FROM contenido_Inventario AS c
-                                INNER JOIN producto AS p ON
-                                 c.producto_id = p.producto_id
-                                    INNER JOIN talla AS t ON
-                                        t.talla_id = p.talla_id
-                                            INNER JOIN pais AS pa ON
-                                                pa.pais_id = t.pais_id
-                                                 WHERE c.stock = 0;
+        GO
+		CREATE VIEW stock_cero_vw AS 
+            SELECT c.Inventario_Id AS Codigo_Inventario, c.Producto_id AS Codigo_producto, 
+                p.tipo + ' ' + p.marca + ' ' + p.Coleccion_temporada + ' ' + p.genero AS producto,
+                    pa.nombre AS Origen, t.numero AS Talla, c.stock  
+                        FROM contenido_Inventario AS c
+                            INNER JOIN producto AS p ON c.producto_id = p.producto_id
+                                INNER JOIN talla AS t ON t.talla_id = p.talla_id
+                                    INNER JOIN pais AS pa ON pa.pais_id = t.pais_id
+                                        WHERE c.stock = 0;
+
     /* VISTA QUE MUESTRA LOS PRODUCTOS QUE CAMBIARON DE PRECIO */
-        CREATE OR REPLACE VIEW variacion_precios_vw AS
-            SELECT p.producto_id, CONCAT(p.tipo," ", p.marca," ", p.Coleccion_temporada," ", p.genero) AS producto,
-                    a.Valor_Venta AS Precio_Anterior, p.Valor_Venta AS Precio_Actual, 
-                        a.ultima_modificacion AS Fecha_Modificacion 
+        GO      
+        CREATE VIEW variacion_precios_vw AS
+            SELECT TOP 100 PERCENT
+                p.producto_id, CONCAT(p.tipo, ' ', p.marca, ' ', p.Coleccion_temporada, ' ', p.genero) AS producto,
+                    a.Valor_Venta AS Precio_Anterior, p.Valor_Venta AS Precio_Actual,
+                        a.ultima_modificacion AS Fecha_Modificacion
                             FROM producto AS p
-                                INNER JOIN antiguo_producto AS a ON
-                                    p.producto_id = a.producto_id
-                                        ORDER BY a.ultima_modificacion DESC;
+                                INNER JOIN antiguo_producto AS a ON p.producto_id = a.producto_id
+                                    ORDER BY a.ultima_modificacion DESC;
                     
     
 
