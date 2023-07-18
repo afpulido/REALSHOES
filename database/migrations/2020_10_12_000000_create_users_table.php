6efbf2e1303793e4d35edf46f55bd7b3e59b7ff5
Omar <ofbohorquez1.@soy.sena.edu.co>
2023-07-17 20:52:32 -0500

    Corrección de create usuario

--- database/migrations/2020_10_12_000000_create_users_table.php
+++ database/migrations/2020_10_12_000000_create_users_table.php
@@ -13,19 +13,37 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->integer('persona_id');
-            $table->string('name');
-            $table->string('apellidos');
-            $table->string('direccion');
-            $table->integer('telefono');
+            $table->string('name');     
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->unsignedBigInteger('tipo_doc_id');
-            $table->unsignedBigInteger('rol_id');
             $table->rememberToken();
             $table->timestamps();
-            
+            $table->integer('persona_id')->nullable();;
+            $table->string('apellidos')->nullable();;
+            $table->string('direccion')->nullable();;
+            $table->integer('telefono')->nullable();;
+            $table->unsignedBigInteger('tipo_doc_id')->nullable();;
+            $table->unsignedBigInteger('rol_id')->nullable();;
+
+        /**
+         * ->nullable();=null
+         * 
+        * $table->id();
+        *   $table->integer('persona_id');
+        *  $table->string('name');
+        * $table->string('apellidos');
+        *   $table->string('direccion');
+        *   $table->integer('telefono');
+        *    $table->string('email')->unique();
+        *   $table->timestamp('email_verified_at')->nullable();
+        *   $table->string('password');
+        *   $table->unsignedBigInteger('tipo_doc_id');
+        *   $table->unsignedBigInteger('rol_id');
+        *   $table->rememberToken();
+        *   $table->timestamps();
+        *
+        */
 
 
         });
